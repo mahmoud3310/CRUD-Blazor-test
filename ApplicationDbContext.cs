30f42ff5f356bd7d2badbc9ee769e68c6766dc06
mahmoud3310 <58104048+mahmoud3310@users.noreply.github.com>
2021-03-18 21:17:48 +0100

    Add files via upload

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyAPI.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyAPI
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<EmployeeInfo> Employees { get; set; }
+    }
+}
---
+++ EmployeeService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyAPI.Model
+{
+    //public class EmployeeService
+
+    //{
+    //    private readonly ApplicationDbContext _db;
+
+    //    public EmployeeService(ApplicationDbContext db)
+    //    {
+    //        _db = db;
+    //    }
+
+    //    //Get all empolyee
+    //    public List<EmployeeInfo> GetEmployee()
+    //    {
+    //        var empList = _db.Employees.ToList();
+    //        return empList;
+    //    }
+
+    //    //insert new employee
+
+    //    public string Create(EmployeeInfo emp)
+    //    {
+    //        _db.Employees.Add(emp);
+    //        _db.SaveChanges();
+    //        return "Save Successfly";
+    //    }
+
+    //    //get by Id 
+    //    public EmployeeInfo GetEmployeeById(Guid ID)
+    //    {
+    //        EmployeeInfo emp = _db.Employees.FirstOrDefault(s => s.ID == ID);
+    //        return emp;
+    //    }
+
+    //    //update
+
+    //    public string UpdateEmployee(EmployeeInfo emp)
+    //    {
+    //        _db.Employees.Update(emp);
+    //        _db.SaveChanges();
+    //        return "Updated";
+    //    }
+
+    //    //delete
+    //    public string DeleteEmployee(EmployeeInfo emp)
+    //    {
+    //        _db.Employees.Remove(emp);
+    //        _db.SaveChanges();
+    //        return "Deleted";
+    //    }
+    //}
+}
